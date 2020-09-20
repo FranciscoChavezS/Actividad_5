c5e1c25bf4506ab08d8f5d0cc306335ce1d3108f
Francisco Chávez <francisco.csalado@alumnos.udg.mx>
2020-09-20 15:20:17 -0500

    Capturamos nombre y edad e imprimimos los datos

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hola que tal";
-    
+    char nombre[100];
+    int edad;
+    cout<<"Hola que tal"<<endl;;
+    cout<<"Nombre: ";
+    cin>>nombre;
+    cout<<"Edad: ";
+    cin>>edad;
+    cout<<"Mi nombre es "<<nombre<<" tengo "<<edad<<" anios de edad "<<endl; 
+
     return 0;
 }
