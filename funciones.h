b8b3ce633967e9d25930cb1015a7dded204199cb
Francisco Chávez <francisco.csalado@alumnos.udg.mx>
2020-09-23 19:54:45 -0500

    implementamos la mult() y resta()

--- funciones.h
+++ funciones.h
@@ -1,13 +1,13 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-void suma(int a, int b)
+int mult(int a, int b)
 {
-
+    return a*b;
 }
-void resta(int a, int b)
+int resta(int a, int b)
 {
-
+    return a - b;
 }
 
 
